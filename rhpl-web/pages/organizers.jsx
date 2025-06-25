4d2eea3b4aed1dc3c1ca87a2bc0a95d0fcf62d9f
Rajiv Dalal <115440094+RajivDalal@users.noreply.github.com>
2025-06-25 14:52:15 +0530

    Delete rhpl-web directory

--- rhpl-web/pages/organizers.jsx
+++
@@ -1,5 +0,0 @@
-const Organizers = () => {
-  return <h1>Organizers Page</h1>;
-};
-
-export default Organizers;
--- rhpl-web/pages/posters.jsx
+++
@@ -1,5 +0,0 @@
-const Posters = () => {
-  return <h1>Posters Page</h1>;
-};
-
-export default Posters;

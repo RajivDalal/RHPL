10f7480089029adcd32c840f324f65d037621448
Rajiv Dalal <115440094+RajivDalal@users.noreply.github.com>
2025-06-27 18:06:35 +0530

    Add files via upload

---
+++ rhpl-web/src/pages/prev_editions.jsx
@@ -0,0 +1,21 @@
+const Previous_editions = () => {
+  return(
+  <div className="content">
+    <p className="pt-10 org">
+      <ul className="list-decimal">
+        <li>
+          <a href="https://fmindia.cmi.ac.in/rhpl2024/"> RHPL 2024 </a> (Third edition)
+        </li>
+        <li>
+          <a href="https://fmindia.cmi.ac.in/rhpl/"> RHPL 2023 </a> (Second edition)
+        </li>
+        <li>
+          <a href="https://www.microsoft.com/en-us/research/event/research-highlights-in-programming-languages-fsttcs-2020/"> RHPL 2020 </a> (Inaugural edition)
+        </li>
+      </ul>
+    </p>
+  </div>
+  )
+};
+
+export default Previous_editions;
---
+++ rhpl-web/src/pages/registration.jsx
@@ -0,0 +1,11 @@
+const Registration = () => {
+  return (
+    <div className="content">
+      <p className="pt-10">
+        TBA
+      </p>
+    </div>
+  )
+};
+
+export default Registration;

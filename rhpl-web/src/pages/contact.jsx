48dcd7f4b589c6058191a1ea5b04af16a73bf0f7
Abhisekh Sankaran <2742953@tcs.com>
2025-06-28 11:30:55 +0530

    Added email addresses in contact page

--- rhpl-web/src/pages/contact.jsx
+++ rhpl-web/src/pages/contact.jsx
@@ -2,7 +2,7 @@ const Contact = () => {
   return (
     <div className="content">
       <p className="pt-10">
-        Please write to Divyesh Unadkat or Abhisekh Sankaran for any queries.
+        Please write to Divyesh Unadkat (divyesh.unadkat@synopsis.com) or Abhisekh Sankaran (abhisekh.sankaran@tcs.com) for any queries.
       </p>
     </div>
   );

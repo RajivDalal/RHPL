8806edc21b9730c95f762527b6386e5b1d1e23b9
Rajiv Dalal <rajiv.dalal2022@vitstudent.ac.in>
2025-06-28 11:21:37 +0530

    fix: Changes 2

--- rhpl-web/src/pages/organizers.jsx
+++ rhpl-web/src/pages/organizers.jsx
@@ -1,8 +1,4 @@
-import { VscLinkExternal } from "react-icons/vsc";
-import { LuExternalLink } from "react-icons/lu";
-import { FiExternalLink } from "react-icons/fi";
 import { RiExternalLinkLine } from "react-icons/ri";
-import { HiExternalLink } from "react-icons/hi";
 const Organizers = () => {
   return (
     <div className="content">
@@ -19,6 +15,9 @@ const Organizers = () => {
           </li>
           <li>
             <a href="https://kartiknagar.github.io/"><span className="underline"> Kartik Nagar</span> (IIT Madras) <RiExternalLinkLine className="inline"/></a>
+          </li>
+          <li>
+            <a href="https://www.csa.iisc.ac.in/~https://sumanthsprabhu.github.io//"> <span className="underline">Sumanth Prabhu</span> (TCS Research) <RiExternalLinkLine className="inline"/> </a>
           </li>
           <li>
             <a href="https://www.microsoft.com/en-us/research/people/grama/"><span className="underline"> Ganesan Ramalingam</span> (Microsoft) <RiExternalLinkLine className="inline"/></a>

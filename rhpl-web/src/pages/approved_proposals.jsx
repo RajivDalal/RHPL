b888bf3972319abe246a72c09ad86a542f9b0136
Rajiv Dalal <115440094+RajivDalal@users.noreply.github.com>
2025-06-30 15:22:23 +0530

    Added rhpl-web

---
+++ rhpl-web/src/pages/approved_proposals.jsx
@@ -0,0 +1,10 @@
+const Approved_proposals = () => {
+  return (
+    <div className="content">
+      <p className="py-10">
+        TBA
+      </p>
+    </div>);
+};
+
+export default Approved_proposals;

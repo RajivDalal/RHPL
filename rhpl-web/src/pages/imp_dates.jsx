5f72a077b5a81427b1dc880e2a39090ab4bfd124
abhisekhs <55447191+abhisekhs@users.noreply.github.com>
2025-07-11 12:26:25 +0530

    Added dates to the fields

--- rhpl-web/src/pages/imp_dates.jsx
+++ rhpl-web/src/pages/imp_dates.jsx
@@ -7,19 +7,19 @@ const Important_dates = () => {
         <hr className="border-black pb-6" />
         <ul className="list-disc space-y-1 ml-8">
           <li>
-            Submission portal opens: Some time in July 2025
+            Submission portal opens: Some time in July 2025, say 3rd week
           </li>
           <li>
-            Submission deadline: Early September
+            Submission deadline: September first week
           </li>
-          {/* <li>
-            Submission deadline extension (provisioning for it): 7 - 10 days
-          </li> */}
-          {/* <li>
-            Review period: 1 week
-          </li> */}
+         <li>
+            [Internal: Submission deadline extension (provisioning for it): 7 - 10 days]
+          </li>
           <li>
-            Notification: September end of 3rd week
+            [Internal: Review period: 1 week]
+          </li>
+          <li>
+            Notification: September end of 3rd week, latest September 25
           </li>
           <li>
             Early registration deadline: TBA

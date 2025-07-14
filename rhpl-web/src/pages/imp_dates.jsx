70e43b79326fb1d0a23674a3df710bd1ece7d75b
abhisekhs <55447191+abhisekhs@users.noreply.github.com>
2025-07-14 10:02:21 +0530

    Updated important dates based on suggestions by senior members

--- rhpl-web/src/pages/imp_dates.jsx
+++ rhpl-web/src/pages/imp_dates.jsx
@@ -7,19 +7,19 @@ const Important_dates = () => {
         <hr className="border-black pb-6" />
         <ul className="list-disc space-y-1 ml-8">
           <li>
-            Submission portal opens: Some time in July 2025, say 3rd week
+            Submission portal opens: Some time in July 2025, say by July 25
           </li>
           <li>
-            Submission deadline: September first week
+            Submission deadline: Soon after Sep 15 (FSTTCS notification deadline) 
           </li>
          <li>
-            [Internal: Submission deadline extension (provisioning for it): 7 - 10 days]
+            [Internal: Submission deadline extension (provisioning for it): at most 7 days]
           </li>
           <li>
             [Internal: Review period: 1 week]
           </li>
           <li>
-            Notification: September end of 3rd week, latest September 25
+            Notification: September last week
           </li>
           <li>
             Early registration deadline: TBA

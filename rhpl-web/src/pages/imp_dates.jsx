dba5f1d9a29d410017bb89db7244618be0dc4e8b
abhisekhs <55447191+abhisekhs@users.noreply.github.com>
2025-07-26 19:03:59 +0530

    Added underline class to link for IST.

--- rhpl-web/src/pages/imp_dates.jsx
+++ rhpl-web/src/pages/imp_dates.jsx
@@ -5,7 +5,7 @@ const Important_dates = () => {
     <p className="py-10 space-y-2">
         <h1 className="text-[27px] font-bold">Important Dates</h1>
         <hr className="border-black pb-6" />
-        All dates below are in <a href="https://en.wikipedia.org/wiki/Indian_Standard_Time">Indian Standard Time (IST)</a>. 
+        All dates below are in <a classname="underline" href="https://en.wikipedia.org/wiki/Indian_Standard_Time">Indian Standard Time (IST)</a>. 
         <ul className="list-disc space-y-1 ml-8">
           <li>
             Submission portal opens: July 28, 2025

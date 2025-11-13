2413fd2b796f52bcdd1df8f2599281bfa1173ea5
abhisekhs <55447191+abhisekhs@users.noreply.github.com>
2025-11-13 17:13:47 +0530

    Updated registration page

--- rhpl-web/src/pages/registration.jsx
+++ rhpl-web/src/pages/registration.jsx
@@ -1,11 +1,16 @@
 const Registration = () => {
   return (
     <div className="content">
-      <p className="py-10">
-        TBA
+      
+      <p className="py-10 paraLink">
+        <h1 className="text-[27px] font-bold">Venue</h1>
+        <hr className="border-black pb-4"/>
+          
+        The workshop will be held at <a className="underline" href="https://www.bits-pilani.ac.in/goa/" >BITS Pilani, K K Birla Goa Campus</a>.
       </p>
-    </div>
+   </div>
   )
 };
+
 
 export default Registration;

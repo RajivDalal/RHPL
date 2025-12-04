8b9c0f73e7a05cad77130ef914d85b4811c1b9e7
Rajiv Dalal <rajiv.dalal2022@vitstudent.ac.in>
2025-12-05 03:54:31 +0530

    feat: Added Tabs to program

--- rhpl-web/src/pages/programme.jsx
+++ rhpl-web/src/pages/programme.jsx
@@ -1,3 +1,6 @@
+import React, { useState } from "react";
+import Tabs from "../components/tabs.jsx";
+
 const ProgrammeRow = ({ time, title }) => {
   return (
     <div className="w-full m-1 flex bg-dayBlue p-1 rounded">
@@ -9,29 +12,48 @@ const ProgrammeRow = ({ time, title }) => {
 
 const SessionHeader = ({ time, number, title, chair, location }) => {
   return (
-    <>
     <div className="w-full m-1 bg-dayBlue p-1 rounded">
-      <div className="flex items-center" >
+      <div className="flex items-center">
         <span className="w-41 text-left">{time}</span>
         <div className="flex flex-col">
-          <span className="flex-1 pl-5 font-bold">Session {number}: {title}</span>
-          <span className="block flex-1 pl-5 font-medium"><b>Chair:</b> {chair}</span>
-          <span className="block flex-1 pl-5 font-medium"><b>Location:</b> {location}</span>
-        </div>        
+          <span className="flex-1 pl-5 font-bold">
+            Session {number}: {title}
+          </span>
+          {chair && (
+            <span className="block flex-1 pl-5 font-medium">
+              <b>Chair:</b> {chair}
+            </span>
+          )}
+          {location && (
+            <span className="block flex-1 pl-5 font-medium">
+              <b>Location:</b> {location}
+            </span>
+          )}
+        </div>
       </div>
     </div>
-    </>
   );
 };
 
-const Talk = ({ time, presenterLink = "#" , presenter ,title, abstract = " ", affiliation = "" }) => {
+const Talk = ({
+  time,
+  presenterLink = "#",
+  presenter,
+  title,
+  abstract = " ",
+  affiliation = "",
+}) => {
   return (
     <div className="flex gap-4">
       <span className="w-40 pl-1">{time}</span>
       <div className="flex flex-col">
-        <span className="italic">
-          <a href={presenterLink} className="underline text-blue-700">{presenter}</a>
-        </span>
+        {presenter && (
+          <span className="italic">
+            <a href={presenterLink} className="underline text-blue-700">
+              {presenter}
+            </a>
+          </span>
+        )}
         <span>{title}</span>
       </div>
       <div>
@@ -45,183 +67,161 @@ const Talk = ({ time, presenterLink = "#" , presenter ,title, abstract = " ", af
 };
 
 const Programme = () => {
+  const [activeDay, setActiveDay] = useState("monday");
+
+  const dayTabs = [
+    { id: "monday", label: "Monday, 17th December" },
+    { id: "tuesday", label: "Tuesday, 18th December" },
+    { id: "wednesday", label: "Wednesday, 19th December" },
+  ];
+
   return (
     <div className="content">
-
       <p className="contain py-10 space-y-2">
+        <h1 className="text-[27px] font-bold">Programme</h1>
+        <hr className="border-black pb-4" />
 
-      <h1 className="text-[27px] font-bold">Programme</h1>
-      
-      <hr className="border-black pb-4"/>
+        <Tabs tabs={dayTabs} activeTab={activeDay} setActiveTab={setActiveDay} />
 
-      {/* Day Header */}
-      <div className="m-1 bg-dayBlue text-center w-full p-1 font-semibold">
-        Monday, 17th December
-      </div>
+        <div className="pt-5">
+        {/* MONDAY PROGRAMME */}
+        {activeDay === "monday" && (
+          <div className="space-y-2">
+            {/* Invited Talk 1 */}
+            <ProgrammeRow
+              time="09:00 AM - 10:00 AM"
+              title="Invited Talk 1"
+            />
+            <div>Dummy text</div>
 
-      {/* Invited Talk 1 */}
-      <ProgrammeRow 
-        time="09:00 AM - 10:00 AM"
-        title="Invited Talk 1"
-      />
-      <div>Dummy text</div>
+            {/* Coffee Break */}
+            <ProgrammeRow
+              time="10:00 AM - 10:30 AM"
+              title="Coffee Break"
+            />
 
-      {/* Coffee Break */}
-      <ProgrammeRow 
-        time="10:00 AM - 10:30 AM"
-        title="Coffee Break"
-      />
+            {/* Morning Session */}
+            <SessionHeader
+              time="10:30 AM - 12:45 PM"
+              number={1}
+              title="Modal Logic"
+              chair="Amaldev Manuel"
+              location="Platinum Jubilee Auditorium"
+            />
 
-      {/* Morning Session */}
-      <SessionHeader 
-        time="10:30 AM - 12:45 PM"
-        number={1}
-        title="Modal Logic"
-        chair="Amaldev Manuel"
-        location="Platinum Jubilee Auditorium"
-      />
+            {/* Inside Morning Session */}
+            <div className="w-full m-1 pl-10 space-y-2">
+              <Talk
+                time="10:30 AM - 10:55 AM"
+                presenter="Presenter"
+                title="Regular Talk 1 Updated"
+              />
+              <Talk
+                time="10:55 AM - 11:20 AM"
+                presenter="Presenter"
+                title="Regular Talk 2 Updated"
+              />
+              <Talk
+                time="11:20 AM - 11:35 AM"
+                presenter="Presenter"
+                title="Short Talk"
+              />
 
-      {/* Inside Morning Session */}
-      <div className="w-full m-1 pl-10 space-y-2">
+              <ProgrammeRow
+                time="11:35 AM - 11:45 AM"
+                title="Short Break"
+              />
 
-        {/* Talk 1 */}
-        <Talk
-          time="10:30 AM - 10:55 AM"
-          presenter="Presenter"
-          title="Regular Talk 1 Updated"
-        />
+              <Talk
+                time="11:45 AM - 12:10 PM"
+                presenter="Presenter"
+                title="Regular Talk 3"
+              />
+              <Talk
+                time="12:10 PM - 12:35 PM"
+                presenter="Presenter"
+                title="Regular Talk 4"
+              />
+            </div>
 
-        {/* Talk 2 */}
-        <Talk
-          time="10:55 AM - 11:20 AM"
-          presenter="Presenter"
-          title="Regular Talk 1 Updated"
-        />
+            {/* Lunch */}
+            <ProgrammeRow
+              time="12:30 PM - 14:00 PM"
+              title="Lunch"
+            />
 
-        {/* Short Talk */}
-        <div className="flex gap-4">
-          <span className="w-40 pl-1">11:20 AM - 11:35 AM</span>
-          <div className="flex flex-col">
-            <span className="italic">
-              <a href="#" className="underline text-blue-700">Presenter</a>
+            {/* Invited Talk 2 */}
+            <ProgrammeRow
+              time="14:00 PM - 15:00 PM"
+              title="Invited Talk 2"
+            />
+            <div>Dummy text</div>
+
+            {/* Coffee Break */}
+            <ProgrammeRow
+              time="15:00 PM - 15:30 PM"
+              title="Coffee Break"
+            />
+
+            {/* Afternoon Session */}
+            <SessionHeader
+              time="15:30 PM - 17:00 PM"
+              number={2}
+              title="Afternoon Session"
+            />
+
+            <div className="w-full m-1 pl-10 space-y-2">
+              <Talk
+                time="15:30 PM - 15:55 PM"
+                presenter="Presenter"
+                title="Regular Talk 5"
+              />
+              <Talk
+                time="15:55 PM - 16:20 PM"
+                presenter="Presenter"
+                title="Regular Talk 6"
+              />
+              <Talk
+                time="16:25 PM - 16:50 PM"
+                presenter="Presenter"
+                title="Regular Talk 7"
+              />
+              <Talk
+                time="16:50 PM - 17:05 PM"
+                presenter="Presenter"
+                title="Short Talk"
+              />
+            </div>
+
+            {/* Reminiscence */}
+            <ProgrammeRow
+              time="17:15 PM - 18:00 PM"
+              title="Reminiscence Session for Venky"
+            />
+            <div>Dummy text</div>
+          </div>
+        )}
+
+        {/* TUESDAY PROGRAMME (placeholder for now) */}
+        {activeDay === "tuesday" && (
+          <div className="m-1">
+            <span className="block font-medium">
+              Programme for Tuesday will be updated soon.
             </span>
-            <span>Short Talk</span>
           </div>
-        </div>
+        )}
 
-        {/* Break */}
-        <div className="flex gap-4">
-          <span className="w-40 pl-1">11:35 AM - 11:45 AM</span>
-          <div className="flex flex-col">
-            <span>Short Break</span>
-          </div>
-        </div>
-
-        {/* Talk 3 */}
-        <div className="flex gap-4">
-          <span className="w-40 pl-1">11:45 AM - 12:10 PM</span>
-          <div className="flex flex-col">
-            <span className="italic">
-              <a href="#" className="underline text-blue-700">Presenter</a>
+        {/* WEDNESDAY PROGRAMME (placeholder for now) */}
+        {activeDay === "wednesday" && (
+          <div className="m-1">
+            <span className="block font-medium">
+              Programme for Wednesday will be updated soon.
             </span>
-            <span>Regular Talk 3</span>
           </div>
+        )}
+
         </div>
-
-        {/* Talk 4 */}
-        <div className="flex gap-4">
-          <span className="w-40 pl-1" >12:10 PM - 12:35 PM</span>
-          <div className="flex flex-col">
-            <span className="italic">
-              <a href="#" className="underline text-blue-700">Presenter</a>
-            </span>
-            <span>Regular Talk 4</span>
-          </div>
-        </div>
-      </div>
-
-      {/* Lunch */}
-      <ProgrammeRow
-        time="12:30 PM - 14:00 PM"
-        title="Lunch"
-      />
-
-      {/* Invited Talk 2 */}
-      <ProgrammeRow
-        time="14:00 PM - 15:00 PM"
-        title="Invited Talk 2"
-      />
-      <div>Dummy text</div>
-
-      {/* Coffee Break */}
-      <ProgrammeRow
-        time="15:00 PM - 15:30 PM"
-        title="Coffee Break"
-      />
-
-      {/* Afternoon Session */}
-      <ProgrammeRow
-        time="15:30 PM - 17:00 PM"
-        title="Afternoon Session"
-      />
-
-      {/* Inside Afternoon Session */}
-      <div className="w-full m-1 pl-10 space-y-2">
-
-        {/* Talk 5 */}
-        <div className="flex gap-4">
-          <span className="w-40 pl-1">15:30 PM - 15:55 PM</span>
-          <div className="flex flex-col">
-            <span className="italic">
-              <a href="#" className="underline text-blue-700">Presenter</a>
-            </span>
-            <span>Regular Talk 5</span>
-          </div>
-        </div>
-
-        {/* Talk 6 */}
-        <div className="flex gap-4">
-          <span className="w-40 pl-1">15:55 PM - 16:20 PM</span>
-          <div className="flex flex-col">
-            <span className="italic">
-              <a href="#" className="underline text-blue-700">Presenter</a>
-            </span>
-            <span>Regular Talk 6</span>
-          </div>
-        </div>
-
-        {/* Talk 7 */}
-        <div className="flex gap-4">
-          <span className="w-40 pl-1">16:25 PM - 16:50 PM</span>
-          <div className="flex flex-col">
-            <span className="italic">
-              <a href="#" className="underline text-blue-700">Presenter</a>
-            </span>
-            <span>Regular Talk 7</span>
-          </div>
-        </div>
-
-        {/* Short Talk */}
-        <div className="flex gap-4">
-          <span className="w-40 pl-1">16:50 PM - 17:05 PM</span>
-          <div className="flex flex-col">
-            <span className="italic">
-              <a href="#" className="underline text-blue-700">Presenter</a>
-            </span>
-            <span>Short Talk</span>
-          </div>
-        </div>
-      </div>
-
-      {/* Reminiscence */}
-      <ProgrammeRow
-        time="17:15 PM - 18:00 PM"
-        title="Reminiscence Session for Venky"
-      />
-      {/* Color Code Later */}
-      <div>Dummy text</div>
-
-    </p>
+      </p>
     </div>
   );
 };

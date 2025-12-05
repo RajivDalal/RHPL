1a646be2552b020c56f2ad35f55dd31b45b83a55
Nidhig19 <nidhigunjal19@gmail.com>
2025-12-05 17:43:45 +0530

    Added data for 3 days

--- rhpl-web/src/pages/programme.jsx
+++ rhpl-web/src/pages/programme.jsx
@@ -3,33 +3,31 @@ import Tabs from "../components/tabs.jsx";
 
 const ProgrammeRow = ({ time, title }) => {
   return (
-    <div className="w-full m-1 flex bg-dayBlue p-1 rounded">
-      <span className="w-41 text-left">{time}</span>
-      <span className="flex-1 pl-5 font-medium">{title}</span>
+    <div className="w-full flex bg-[#c2e0f4] p-3 rounded mb-2">
+      <span className="w-48 flex-shrink-0 text-left font-medium">{time}</span>
+      <span className="flex-1 pl-4 font-semibold">{title}</span>
     </div>
   );
 };
 
 const SessionHeader = ({ time, number, title, chair, location }) => {
   return (
-    <div className="w-full m-1 bg-dayBlue p-1 rounded">
-      <div className="flex items-center">
-        <span className="w-41 text-left">{time}</span>
-        <div className="flex flex-col">
-          <span className="flex-1 pl-5 font-bold">
-            Session {number}: {title}
-          </span>
-          {chair && (
-            <span className="block flex-1 pl-5 font-medium">
-              <b>Chair:</b> {chair}
-            </span>
-          )}
-          {location && (
-            <span className="block flex-1 pl-5 font-medium">
-              <b>Location:</b> {location}
-            </span>
-          )}
-        </div>
+    <div className="w-full flex bg-[#c2e0f4] p-3 rounded mb-2">
+      <span className="w-48 flex-shrink-0 text-left font-medium">{time}</span>
+      <div className="flex-1 pl-4">
+        <span className="font-bold">
+          Session {number}: {title}
+        </span>
+        {chair && (
+          <div className="mt-1">
+            <span className="font-semibold">Chair:</span> {chair}
+          </div>
+        )}
+        {location && (
+          <div className="mt-1">
+            <span className="font-semibold">Location:</span> {location}
+          </div>
+        )}
       </div>
     </div>
   );
@@ -44,23 +42,17 @@ const Talk = ({
   affiliation = "",
 }) => {
   return (
-    <div className="flex gap-4">
-      <span className="w-40 pl-1">{time}</span>
-      <div className="flex flex-col">
+    <div className="flex py-2">
+      <span className="w-48 flex-shrink-0 text-left">{time}</span>
+      <div className="flex-1 pl-4">
         {presenter && (
-          <span className="italic">
-            <a href={presenterLink} className="underline text-blue-700">
+          <div className="italic mb-1">
+            <a href={presenterLink} className="underline text-blue-700 hover:text-blue-900">
               {presenter}
             </a>
-          </span>
+          </div>
         )}
-        <span>{title}</span>
-      </div>
-      <div>
-        <span className="hidden w-40 pl-1">{abstract}</span>
-      </div>
-      <div>
-        <span className="hidden w-40 pl-1">{affiliation}</span>
+        <div>{title}</div>
       </div>
     </div>
   );
@@ -90,7 +82,7 @@ const Programme = () => {
             {/* Invited Talk 1 */}
             <ProgrammeRow
               time="09:00 AM - 10:00 AM"
-              title="Invited Talk 1"
+              title="FSTTCS Invited Talk"
             />
             <div>Dummy text</div>
 
@@ -102,7 +94,7 @@ const Programme = () => {
 
             {/* Morning Session */}
             <SessionHeader
-              time="10:30 AM - 12:45 PM"
+              time="10:30 AM - 12:35 PM"
               number={1}
               title="Concurrency"
               chair="TBA"
@@ -113,18 +105,18 @@ const Programme = () => {
             <div className="w-full m-1 pl-10 space-y-2">
               <Talk
                 time="10:30 AM - 10:55 AM"
-                presenter="Presenter"
-                title="Regular Talk 1 Updated"
+                presenter="Nobuko Yoshida"
+                title="Specification Guided Distributed Programming"
               />
               <Talk
                 time="10:55 AM - 11:20 AM"
-                presenter="Presenter"
-                title="Regular Talk 2 Updated"
+                presenter="Omkar Tuppe"
+                title="GPUMC: A Stateless Model Checker for GPU Weak Memory Concurrency"
               />
               <Talk
                 time="11:20 AM - 11:35 AM"
-                presenter="Presenter"
-                title="Short Talk"
+                presenter="Tephilla Prince"
+                title="Bridging Nets-within-Nets and Data Nets"
               />
             </div>
 
@@ -140,26 +132,26 @@ const Programme = () => {
 
               <Talk
                 time="11:45 AM - 12:10 PM"
-                presenter="Presenter"
-                title="Regular Talk 3"
+                presenter="Prince Mathew"
+                title="Learning Deterministic One-Counter Automata in Polynomial Time3"
               />
               <Talk
                 time="12:10 PM - 12:35 PM"
-                presenter="Presenter"
-                title="Regular Talk 4"
+                presenter="Gourav Takhar"
+                title="Memory-Safety Verification of Open Programs With Angelic Assumptions"
               />
             </div>
 
             {/* Lunch */}
             <ProgrammeRow
-              time="12:30 PM - 14:00 PM"
+              time="12:40 PM - 14:00 PM"
               title="Lunch"
             />
 
             {/* Invited Talk 2 */}
             <ProgrammeRow
               time="14:00 PM - 15:00 PM"
-              title="Invited Talk 2"
+              title="FSTTCS Invited Talk"
             />
             <div>Dummy text</div>
 
@@ -181,13 +173,13 @@ const Programme = () => {
             <div className="w-full m-1 pl-10 space-y-2">
               <Talk
                 time="15:30 PM - 15:55 PM"
-                presenter="Presenter"
-                title="Regular Talk 5"
+                presenter="Ramneet Singh"
+                title="INTERLEAVE: A Faster Symbolic Algorithm for Maximal End Component Decomposition"
               />
               <Talk
                 time="15:55 PM - 16:20 PM"
-                presenter="Presenter"
-                title="Regular Talk 6"
+                presenter="Shibashis Guha"
+                title="Continuous Time Reward Machines"
               />
             </div>
 
@@ -202,52 +194,271 @@ const Programme = () => {
             <div className="w-full m-1 pl-10 space-y-2">
               <Talk
                 time="16:30 PM - 16:45 PM"
-                presenter="Presenter"
-                title="Short Talk"
+                presenter="Atanu Kundu"
+                title="Data-Driven Falsification of Cyber-Physical Systems"
               />
               <Talk
                 time="16:45 PM - 17:00 PM"
-                presenter="Presenter"
-                title="Short Talk"
+                presenter="Anand Yeolekar"
+                title="Repairing Control Safety Violations by Patching the Real-time OS Scheduler"
               />
               <Talk
                 time="17:00 PM - 17:15 PM"
-                presenter="Presenter"
-                title="Short Talk"
+                presenter="Alvin A. George"
+                title="CEGAR for Temporal Properties based on Recurrent Sets"
               />
               <Talk
                 time="17:15 PM - 17:30 PM"
-                presenter="Presenter"
-                title="Short Talk"
+                presenter="Santonu Sarkar"
+                title="LLM-Assisted Formal Verification Framework for Process Control Software Evolution"
               />
             </div>
 
-            {/* Reminiscence */}
-            <ProgrammeRow
-              time="17:15 PM - 18:00 PM"
-              title="Reminiscence Session for Venky"
-            />
+            
             <div>Dummy text</div>
           </div>
         )}
 
         {/* TUESDAY PROGRAMME (placeholder for now) */}
         {activeDay === "tuesday" && (
-          <div className="m-1">
-            <span className="block font-medium">
-              Programme for Tuesday will be updated soon.
-            </span>
+            <div className="space-y-2">
+            {/* Invited Talk 1 */}
+            <ProgrammeRow
+              time="09:00 AM - 10:00 AM"
+              title="FSTTCS Invited Talk"
+            />
+            <div>Dummy text</div>
+
+            {/* Coffee Break */}
+            <ProgrammeRow
+              time="10:00 AM - 10:30 AM"
+              title="Coffee Break"
+            />
+
+            {/* Morning Session */}
+            <SessionHeader
+              time="10:30 AM - 11:35 AMM"
+              number={1}
+              title="Program Equivalence and Neural Network Verification"
+              chair="TBA"
+              location="TBA"
+            />
+
+            {/* Inside Morning Session */}
+            <div className="w-full m-1 pl-10 space-y-2">
+              <Talk
+                time="10:30 AM - 10:55 AM"
+                presenter="Umang Mathur"
+                title="Equivalences for Causal Concurrency"
+              />
+              <Talk
+                time="10:55 AM - 11:20 AM"
+                presenter="S. VenkataKeerthy"
+                title="VEXIR2Vec: An Architecture-Neutral Embedding Framework for Binary Similarity"
+              />
+              <Talk
+                time="11:20 AM - 11:35 AM"
+                presenter="Mohammad Afzal"
+                title="Confidence-aware local robustness verification of neural networks"
+              />
+            </div>
+
+            <SessionHeader
+              time="11:45 AM - 12:40 PM"
+              number={2}
+              title="Compilers"
+              chair="TBA"
+              location="TBA"
+            />
+
+            <div className="w-full m-1 pl-10 space-y-2">
+
+              <Talk
+                time="11:45 AM - 12:10 PM"
+                presenter="Aditya Anand"
+                title="CoSSJIT: Combining Static Analysis and Speculation in JIT Compilers"
+              />
+              <Talk
+                time="12:10 PM - 12:25 PM"
+                presenter="Supriya Bhide"
+                title="CoS-SSA: Context-Sensitive SSA for Interprocedural Program Analyses and Optimisations"
+              />
+              <Talk
+                time="12:25 PM - 12:40 PM"
+                presenter="Aniket Mishra"
+                title="Fall-through Semantics for Mitigating Timing-based Side Channel Leaks"
+              />
+            </div>
+
+            {/* Lunch */}
+            <ProgrammeRow
+              time="12:40 PM - 14:00 PM"
+              title="Lunch"
+            />
+
+            {/* Invited Talk 2 */}
+            <ProgrammeRow
+              time="14:00 PM - 15:00 PM"
+              title="FSTTCS Invited Talk"
+            />
+            <div>Dummy text</div>
+
+            {/* Coffee Break */}
+            <ProgrammeRow
+              time="15:00 PM - 15:30 PM"
+              title="Coffee Break"
+            />
+
+            {/* Afternoon Session */}
+            
+          
           </div>
         )}
 
         {/* WEDNESDAY PROGRAMME (placeholder for now) */}
         {activeDay === "wednesday" && (
-          <div className="m-1">
-            <span className="block font-medium">
-              Programme for Wednesday will be updated soon.
-            </span>
+          <div className="space-y-2">
+            {/* Invited Talk 1 */}
+            <ProgrammeRow
+              time="09:00 AM - 10:00 AM"
+              title="FSTTCS Invited Talk"
+            />
+            <div>Dummy text</div>
+
+            {/* Coffee Break */}
+            <ProgrammeRow
+              time="10:00 AM - 10:30 AM"
+              title="Coffee Break"
+            />
+
+            {/* Morning Session */}
+            <SessionHeader
+              time="10:30 AM - 12:40 PM"
+              number={1}
+              title="Session 1"
+              chair="TBA"
+              location="TBA"
+            />
+
+            {/* Inside Morning Session */}
+            <div className="w-full m-1 pl-10 space-y-2">
+              <Talk
+                time="10:30 AM - 10:45 AM"
+                presenter="Siddharth Priya"
+                title="Ownership in Low-Level Intermediate Representation"
+              />
+              <Talk
+                time="10:45 AM - 11:00 AM"
+                presenter="Dr Varsha Jarali"
+                title="On The Dolev-Yao Model of Symmetric Cascade Protocol"
+              />
+              <Talk
+                time="11:00 AM - 11:15 AM"
+                presenter="Arpita Dutta"
+                title="Incremental and Unbounded Loop Analysis"
+              />
+            </div>
+             {/* Reminiscence */}
+            <ProgrammeRow
+              time="11:30 AM - 12:40 PM"
+              title="Reminiscence Session for Venky"
+            />
+            {/* Lunch */}
+          <ProgrammeRow
+              time="12:40 PM - 14:00 PM"
+              title="Lunch"
+            />
+            
+
+            {/* Invited Talk 2 */}
+            <ProgrammeRow
+              time="14:00 PM - 15:00 PM"
+              title="FSTTCS Invited Talk"
+            />
+            <div>Dummy text</div>
+
+            {/* Coffee Break */}
+            <ProgrammeRow
+              time="15:00 PM - 15:30 PM"
+              title="Coffee Break"
+            />
+
+            {/* Afternoon Session */}
+            <SessionHeader
+              time="15:30 PM - 17:00 PM"
+              number={3}
+              title="Session 3"
+              chair="TBA"
+              location="TBA"
+            />
+
+            <div className="w-full m-1 pl-10 space-y-2">
+              <Talk
+                time="15:30 PM - 15:45 PM"
+                presenter="Presenter"
+                title="short talk"
+              />
+              <Talk
+                time="15:45 PM - 16:00 PM"
+                presenter="Presenter"
+                title="short talk"
+              />
+              <Talk
+                time="16:00 PM - 16:15 PM"
+                presenter="Presenter"
+                title="short talk"
+              />
+              <Talk
+                time="16:15 PM - 16:30 PM"
+                presenter="Presenter"
+                title="regular talk (online)"
+              />
+              <Talk
+                time="16:30 PM - 16:45 PM"
+                presenter="Presenter"
+                title="regular talk (online)"
+              />
+              <Talk
+                time="16:45 PM - 17:00 PM"
+                presenter="Presenter"
+                title="regular talk (online)"
+              />
+            </div>
+
+            <SessionHeader
+              time="17:15 PM - 18:05 PM"
+              number={4}
+              title="Session 4"
+              chair="TBA"
+              location="TBA"
+            />
+
+            <div className="w-full m-1 pl-10 space-y-2">
+              <Talk
+                time="17:15 PM - 17:30 PM"
+                presenter="Presenter"
+                title="regular talk (online)"
+              />
+              <Talk
+                time="17:30 PM - 17:45 PM"
+                presenter="Presenter"
+                title="regular talk (online)"
+              />
+              <Talk
+                time="17:45 PM - 17:50 PM"
+                presenter="Presenter"
+                title="short talk (online)"
+              />
+              <Talk
+                time="17:50 PM - 18:05 PM"
+                presenter="Presenter"
+                title="short talk (online)"
+              />
+            </div>
           </div>
         )}
+
+      
 
         </div>
       </p>
